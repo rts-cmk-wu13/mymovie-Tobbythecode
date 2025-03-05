92215df28a32aa0db568ba94c888375f35ed3f09
Tobias <tobimation23@gmail.com>
2025-03-05 14:11:02 +0100

    added alot of things such as fixing stuff, added multiple stylings and etc

--- HomeMenu.js
+++ HomeMenu.js
@@ -1,11 +1,9 @@
 /**
- * Extract id as string from url to pokemon
- * @param {string} file_path - a url to a pokemon from pokeApi 
+ * 
+ * @param {string} file_path 
  * @returns {string}
  */
-function getIdFromMovie(file_path) {
-    return file_path
-}
+
   const baseUrl = 'https://image.tmdb.org/t/p/w500';
 const options = {
     method: 'GET',
@@ -24,7 +22,7 @@ const options = {
 headerElm.className = "Header_main"
 
 headerElm.innerHTML = `
-<section class="navContainer">
+<section class="navContainer  columns">
 <nav class="Movie__navbar">
     <figure>
     <img src="" alt="placeholder.png">
@@ -37,37 +35,105 @@ headerElm.innerHTML = `
 </button>
 </nav>
 
+</section>
 
-</section>
-</section>
 
 
 `
 document.querySelector("header").append(headerElm)
 ;
 
- let mainELm = document.createElement("section")
+let genreMap = {}; 
 
+
+
+fetch('https://api.themoviedb.org/3/movie/12?language=en-US', options)
+  .then(response => response.json())
+  .then(data => {
+    genreMap = data.genres.reduce((acc, genre) => {
+      acc[genre.id] = genre.name;
+      return acc;
+    }, {});
+  })
+
+let anotherdivElm = document.createElement("div")
+ let SectionELm = document.createElement("section")
+
+ SectionELm.className = "main_scroll"
 fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
 
 .then(function(response) {
     return response.json()
 
-}).then(
+}).then(  
     function(data) {
-   mainELm.innerHTML +=  data.results.map(movie => `
+      
+      SectionELm.innerHTML =  `
+     <div class="columns  popular_headline">
+    <h1>now showing</h1>
+    <button>see more</button>
+  </div>
+    <section class="main_test--scroll colunms" >
+
+      ${data.results.map(movie => `
 <div class="movie_poster-container" >
+
 <article>
-<img src="${baseUrl}/${getIdFromMovie(movie.poster_path)}" alt="">
+<figure class="now-showing_movie__images">
+<a href="/movie_details.html?movie=${movie.id}">
+<img src="${baseUrl}/${movie.poster_path}" alt="">
+</a> 
+
+</figure>
 <p>${movie.original_title}</p>
+<p>${movie.vote_average.toFixed(1)}/10 IMDb</p>
 </article>
 </div>
-`).join("")
-document.querySelector("main").append(mainELm)
+`).join("")}    </section>
+`
+document.querySelector("main").append(SectionELm)
 
    
 }
 )
+fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
+.then(function(response) {
+  return response.json()
 
+}).then(
+  function(data) {
+    anotherdivElm.innerHTML = `
+    <div class="columns  popular_headline">
+    <h1>Popular</h1>
+    <button>see more</button>
+  </div>
+    <section>
+${data.results.map(movie => `
+  
+   <div class="movie_poster-container">
+        <article class = "popular_movie--container columns">
+          <figure class="popular-movie__images">
+        <img src="${baseUrl}/${movie.poster_path}" alt="">
+        
+          </figure>
+               <div class="movie__sidetext_info">
+          <p>${movie.original_title}</p>
+          <p>${movie.vote_average.toFixed(1)}/10 IMDb</p>
+          
+ <p >
+                  ${movie.genre_ids.map(id => `<span class="specific__genre--types">${genreMap[id] || "Unknown"}</span>`).join(", ")}
+                </p>
 
-   
+          </div>
+        </article>
+
+      </div>
+  
+  `).join("")}
+    </section>
+    `
+
+document.querySelector("main").append( anotherdivElm)
+
+  })
+
