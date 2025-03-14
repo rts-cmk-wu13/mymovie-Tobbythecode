02113a5c6652c2c9a2675a682125ccc3609a832b
Tobias <tobimation23@gmail.com>
2025-03-14 14:01:57 +0100

    just added a console log here for the #switch

--- script.js
+++ script.js
@@ -2,47 +2,40 @@ let rootElm = document.documentElement;
 let switchElm = document.querySelector("#switch")
 let isDarkMode = readFromLocalStorage("isDarkMode")
 let browserDark = window.matchMedia("(prefers-color-scheme: dark)").matches
-console.log("Matchmedia", browserDark)
-console.log("localstorage",isDarkMode)
+console.log("Matchmedia", browserDark);
+console.log("localStorage", isDarkMode);
+console.log(document.querySelector("#switch"));
+
 
 
 let darkState = null
 
-if( isDarkMode == null){
+if (isDarkMode == null){
     darkState = browserDark;
-}else {
-    darkState = isDarkMode; 
+} else {
+    darkState = isDarkMode;
 }
 
-if( darkState) {
-
-  console.log(" er du her på darkmode, lille ven")
+if(darkState) {
+    console.log("Er du her på darkmode, lille ven??? :)")
     switchElm.checked = true
     rootElm.setAttribute("data-dark", switchElm.checked)
-} else {
-
-
-  
-    console.log("fromlightmode", isDarkMode, !browserDark)
+} 
+else  {
+    console.log("fromLightmode", isDarkMode, !browserDark)
     switchElm.checked = false
     rootElm.setAttribute("data-dark", switchElm.checked)
 } 
 
-
-
-
-
-
-
 switchElm.addEventListener("change", function(){
-console.log(switchElm.checked)
+    console.log(switchElm.checked)
+  
 
-if(switchElm.checked){
-    rootElm.setAttribute("data-dark", switchElm.checked)
-    savetolocalstorage("isDarkMode", switchElm.checked)
-} else {
-    rootElm.setAttribute("data-dark", switchElm.checked)
- savetolocalstorage("isDarkMode", switchElm.checked)
-}
-
+    if(switchElm.checked){
+        rootElm.setAttribute("data-dark", switchElm.checked)
+        saveToLocalStorage("isDarkMode", switchElm.checked)
+    } else {
+        rootElm.setAttribute("data-dark", switchElm.checked)
+        saveToLocalStorage("isDarkMode", switchElm.checked)
+    }
 })
