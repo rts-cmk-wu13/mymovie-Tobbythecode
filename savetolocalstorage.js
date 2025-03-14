92d18ec26339673907d78c20f59e9d4c8258eef1
Tobias <tobimation23@gmail.com>
2025-03-14 13:54:39 +0100

    i kinda forgot what i did here but im pretty sure i just changed "anya" to "any"

--- savetolocalstorage.js
+++ savetolocalstorage.js
@@ -1,37 +1,30 @@
 /**
- * 
- * @typedef {string | number | boolean | object | anya[]} localStorageData - value
+ *  @typedef  {string | number | boolean | object | any[] } LocalStorageData to be saved in localStorage
  */
-
+ 
 /**
- * 
- * 
- * @param {string} key 
- * @param {string | number | boolean | object | anya[]} value 
+ *
+ * @param {string} key - key to be used in local storage
+ * @param {string | number | boolean | object | any[] } value - value to be saved
  * @returns {string}
  */
-
-function savetolocalstorage(key, value) {
-
+function saveToLocalStorage(key, value){
     localStorage.setItem(key, JSON.stringify(value))
     return "Data was saved with the key" + key
 }
-
+ 
+ 
 /**
- * 
- * 
- * @param {string} key 
- * @param {string | number | boolean | object | anya[]} 
  *
+ * @param {string} key
+ * @returns {string | number | boolean | object | any[] }
  */
-function readFromLocalStorage(key){
-
+function readFromLocalStorage(key) {
     return JSON.parse(localStorage.getItem(key))
 }
+ 
 
-
-function deleteFromLocalStorage(key){
-
+function deleteFromLocalStorage(key) {
     localStorage.removeItem(key)
-    return " the element with key " + key + " was deleted";
+    return "The element with key " + key + " was deleted";
 }
